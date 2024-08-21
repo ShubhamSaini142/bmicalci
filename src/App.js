3ec1886b0b31015ad50ee65928c348004d8cee12
Shubham <shubham.sms142@gmail.com>
2024-08-21 15:54:59 +0530

    done with logic

--- src/App.js
+++ src/App.js
@@ -1,43 +1,76 @@
-
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   //states
 
-  const [weight,setWeight] = useState(0)
-  const [height,setHeight] = useState(0)
-  const [message,setMessage] = useState('')
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [message, setMessage] = useState("");
+  const [bmi, setBmi] = useState("");
+
+  // events
+
+  const reload = () =>{
+    window.location.reload()
+
+  }
+
+  const bmiCal = (event) =>{
+    event.preventDefault()
+
+    if(weight == 0 || height == 0){
+      alert("Please Enter Correct values")
+    }
+    else{
+      let bmi =( (weight)/((height) * (height)) ) * 10000
+      setBmi(bmi)
+    }
+
+    if(bmi < 25){
+      setMessage("You are underweight")
+    }else if (bmi >= 25 && bmi < 30) {
+      setMessage('You are a healthy weight')
+    } else {
+      setMessage('You are overweight')
+    }
+
+
+  }
+
+
 
   return (
     <div className="App">
-    <div className="container">
-     <h1 className='center'>BMI CALCULATOR</h1>
-     <form>
-      <div>
-        <label>Weight in Kg</label>
-        <input value={weight} onChange={(e)=> setWeight(e.target.value)}></input>
+      <div className="container">
+        <h1 className="center">BMI CALCULATOR</h1>
+        <form onSubmit={bmiCal}>
+          <div>
+            <label>Weight in Kg</label>
+            <input
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label>Height in Cm</label>
+            <input
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+          <div>
+            <button type="submit" onClick={reload}>Reload</button>
+          </div>
+        </form>
+        <div>
+          <h2>You BMI is:{bmi}</h2>
+         <h3> <p>{message}</p></h3>
+        </div>
       </div>
-      <div>
-        <label>Height in inches</label>
-        <input value={height} onChange={(e) => setHeight(e.target.value)}></input>
-      </div>
-      <div>
-        <button>Submit</button>
-        
-      </div>
-      <div><button>Reload</button></div>
-
-      
-
-     </form>
-     <div>
-      <h2>You BMI is:</h2>
-      <p>{message}</p>
-     </div>
-    </div>
-     
     </div>
   );
 }
