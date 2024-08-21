1f3ae0915b12c5d8d84f4c31a91d2355aaa3dbff
Shubham <shubham.sms142@gmail.com>
2024-08-21 15:41:46 +0530

    UI is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //states
+
+  const [weight,setWeight] = useState(0)
+  const [height,setHeight] = useState(0)
+  const [message,setMessage] = useState('')
+
   return (
     <div className="App">
-      <h1>HEllo world</h1>
+    <div className="container">
+     <h1 className='center'>BMI CALCULATOR</h1>
+     <form>
+      <div>
+        <label>Weight in Kg</label>
+        <input value={weight} onChange={(e)=> setWeight(e.target.value)}></input>
+      </div>
+      <div>
+        <label>Height in inches</label>
+        <input value={height} onChange={(e) => setHeight(e.target.value)}></input>
+      </div>
+      <div>
+        <button>Submit</button>
+        
+      </div>
+      <div><button>Reload</button></div>
+
+      
+
+     </form>
+     <div>
+      <h2>You BMI is:</h2>
+      <p>{message}</p>
+     </div>
+    </div>
+     
     </div>
   );
 }
