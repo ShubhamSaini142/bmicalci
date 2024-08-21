c82fe68a2e5104426c4ea76f46efc3b6fdd9f6ab
Shubham <shubham.sms142@gmail.com>
2024-08-21 16:07:24 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -11,34 +11,28 @@ function App() {
 
   // events
 
-  const reload = () =>{
-    window.location.reload()
+  const reload = () => {
+    window.location.reload();
+  };
 
-  }
+  const bmiCal = (event) => {
+    event.preventDefault();
 
-  const bmiCal = (event) =>{
-    event.preventDefault()
-
-    if(weight == 0 || height == 0){
-      alert("Please Enter Correct values")
-    }
-    else{
-      let bmi =( (weight)/((height) * (height)) ) * 10000
-      setBmi(bmi)
-    }
-
-    if(bmi < 25){
-      setMessage("You are underweight")
-    }else if (bmi >= 25 && bmi < 30) {
-      setMessage('You are a healthy weight')
+    if (weight == 0 || height == 0) {
+      alert("Please Enter Correct values");
     } else {
-      setMessage('You are overweight')
+      let bmi = (weight / (height * height)) * 10000;
+      setBmi(bmi);
     }
 
-
-  }
-
-
+    if (bmi < 25) {
+      setMessage("You are underweight");
+    } else if (bmi >= 25 && bmi < 28) {
+      setMessage("You are a healthy weight");
+    } else {
+      setMessage("You are overweight");
+    }
+  };
 
   return (
     <div className="App">
@@ -60,15 +54,20 @@ function App() {
             ></input>
           </div>
           <div>
-            <button type="submit">Submit</button>
+            <button className= "btn" type="submit">Submit</button>
           </div>
           <div>
-            <button type="submit" onClick={reload}>Reload</button>
+            <button className= "btn" type="submit" onClick={reload}>
+              Reload
+            </button>
           </div>
         </form>
         <div>
           <h2>You BMI is:{bmi}</h2>
-         <h3> <p>{message}</p></h3>
+          <h3>
+            {" "}
+            <p>{message}</p>
+          </h3>
         </div>
       </div>
     </div>
